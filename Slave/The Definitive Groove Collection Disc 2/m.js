7e485aadcbad55c5300f01c3ff5a1723528bcc27
littleflute <13699175164@sina.cn>
2017-07-13 14:28:58 -0500

    Add files via upload

---
+++ Slave/The Definitive Groove Collection Disc 2/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Snap Shot.mp3";		
+f[2]	= "02 Feel My Love.mp3";		
+f[3]	= "03 Wait for Me.mp3";		
+f[4]	= "04 Steal Your Heart.mp3";		
+f[5]	= "05 Party Lites.mp3";		
+f[6]	= "06 Spice of Life (Oh Yes, You're the Best).mp3";		
+f[7]	= "07 For the Love of U.mp3";		
+f[8]	= "08 Way Out.mp3";		
+f[9]	= "09 I'll Be Gone.mp3";		
+f[10]	= "10 Nobody Can Be You.mp3";		
+f[11]	= "11 Weak at the Knees.mp3";		
+f[12]	= "12 You Meet My Approval.mp3";		
+f[13]	= "13 Feel So Real.mp3";		
+f[14]	= "14 Dancin' in the Key of Life.mp3";		
+f[15]	= "Homeboy.mp3";		
